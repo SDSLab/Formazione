8b217d10525afa4b7ba9d73e81029fe5d9000d34
CVarisco <C.Varisco@lutechmobile.com>
2014-04-23 20:17:12 +0200

    23/04 CVHTML
    Ho concluso il cv in formato intero.
    La timeline mi ha dato parecchi prolemi, e non è ancora ottima.
    Domani mi farò sentire perchè ho delle domande sul progetto.

--- cvhtml/container.php
+++ cvhtml/container.php
@@ -31,33 +31,7 @@
 				</div>
 				
 		</div>
-		<div id="timeline">
 		
-			<div id="upSide">
-				<div id="itemUp1">
-				born in Romania
-				<div id="circleTime"><p>1984</p></div>
-				</div>
-				<div id="itemUp2">
-				</div>
-				<div id="itemUp3">
-				</div>
-				<div id="itemUp4">
-				</div>
-			</div>
-			<div id="downSide">
-				<div id="itemDown1">
-				</div>
-				<div id="itemDown2">
-				</div>
-				<div id="itemDown3">
-				</div>
-				<div id="itemDown4">
-				</div>	
-				
-			</div>
-			
-		</div>
 		<div id="designAppr">
 			<p class="titleBlue"> MY DESIGN APPROACH:</p>
 			<div id="circle">
@@ -67,18 +41,142 @@
 			</div>
 		</div>
 		
+		<div id="timeline">
+		
+			<div id="itemUp1">
+			
+				<p>born in Romania</p>
+				<i class="fa fa-square"></i>
+				<div id="circleTime1">
+					<p>1984</p>
+				</div>
+				
+			</div>
+			
+			<div id="itemDown1">
+				
+				<i class="fa fa-square"></i>
+				<p>my first website</p>
+				<div id="circleTime5">
+					<p>2003</p>
+				</div>
+			</div>
+			
+			<div id="itemUp2">
+			
+				<p>born in Romania</p>
+				<i class="fa fa-square"></i>
+				<div id="circleTime2">
+					<p>2004</p>
+				</div>
+			</div>
+			
+			<div id="itemDown2">
+			
+				<i class="fa fa-square"></i>
+				<p>first design job</p>
+				<div id="circleTime6">
+					<p>2005</p>
+				</div>
+			</div>
+			
+			<div id="itemUp3">
+			
+				<p>freelancing</p>
+				<i class="fa fa-square"></i>
+				<div id="circleTime3">
+					<p>2006</p>
+				</div>
+			</div>
+			
+			<div id="itemDown3">
+			
+				<i class="fa fa-square"></i>
+				<p>started to manage projects</p>
+				<div id="circleTime7">
+					<p>2007</p>
+				</div>
+			</div>
+			
+			<div id="itemUp4">
+			
+				<p>became a creative director</p>
+				<i class="fa fa-square"></i>
+				<div id="circleTime4">
+					<p>2008</p>
+				</div>
+			</div>
+			
+			<div id="itemDown4">
+			
+				<i class="fa fa-square"></i>
+				<p>over 250 projects delivered</p>
+				<div id="circleTime8">
+					<p>2013</p>
+				</div>
+			</div>
+			
+			
+			
+		</div>
+
+		
 		
 	</div>
 	
 	<div id="box2">
 	
 		<div id="experience">
+			<i class="fa fa-star-o titleGreen"></i><p class="titleGreen"> EXPERIENCE</p>
+				<div id="listSkills1">
+					<ul>
+					<li>Design Director</li>
+					<li>Creative Director</li>
+					<li>Freelancing Designer</li>
+					<li>Design Director</li>
+					<li>Designer</li>
+					<li>Web Designer</li>
+					</ul>
+				</div>
+				<div id="listSkills2">
+					<ul>
+					<li><p class="colorTextRed">GI Real Estate</p> (2011-2013)</li>
+					<li><p class="colorTextRed">PlayTheBalls</p> (2008-2011)</li>
+					<li><p class="colorTextRed">Global</p> (2006-2013)</li>
+					<li><p class="colorTextRed">ASII</p> (2004-2006)</li>
+					<li><p class="colorTextRed">BWWC</p> (2006)</li>
+					<li><p class="colorTextRed">Desys</p> (2005)</li>
+					</ul>
+				</div>
 		</div>
 		<div id="education">
+			<i class="fa fa-star-o titleBlue"></i><p class="titleBlue"> EDUCATION</p>
+				<div id="textEducation">
+					<ul>
+					<li><p class="colorTextBlue">2011 EXEC EDU</li>
+					<li>Excellence in Management Programme</li>
+					</ul>
+					<ul>
+					<li><p class="colorTextBlue">2008 "George Enescu" Art & Design University</li>
+					<li>BA in Graphic Design and Visual Communication</li>
+					</ul>
+					<ul>
+					<li><p class="colorTextBlue">2003 "Unirea" National College</li>
+					<li>Computer Operator high school diploma</li>
+					</ul>
+				</div>
 		</div>
 		<div id="softSkills">
+			<i class="fa fa-rocket  titleBlue"></i><p class="titleBlue">SOFT SKILLS</p><br>
+			<p>Proactive, entrepreneur attitude, problem solver, emotional intelligence,left & right brain user, good organizer.</p>
+			
 		</div>
 		<div id="passion">
+			<i class="fa fa-heart-o  titleGreen"></i><p class="titleGreen">PASSION</p><br>
+			<p><i class="fa fa-plane"></i><i class="fa fa-pencil"></i>
+			<i class="fa fa-mobile"></i>  <i class="fa fa-picture-o"></i>
+			<i class="fa fa-globe"></i> <i class="fa fa-film"></i>
+			<i class="fa fa-anchor"></i><i class="fa fa-music"></i></p>
 		</div>
 	
 	</div>
--- cvhtml/head.php
+++ cvhtml/head.php
@@ -8,9 +8,15 @@
   <link href="css/css/container.css" rel="stylesheet" type="text/css">
   <link href="css/css/designApproach.css" rel="stylesheet" type="text/css">
   <link href="css/css/aboutskills.css" rel="stylesheet" type="text/css">
+  <link href="css/css/box2.css" rel="stylesheet" type="text/css">
   <link href="css/css/timeline.css" rel="stylesheet" type="text/css">
    <link href="css/css/animation.css" rel="stylesheet" type="text/css">
+   
+   <!-- MEDIA CSS -->
+   <link rel="stylesheet" type="text/css" media="screen and (max-width:21cm),only screen and (max-device-width: 480px)" href="css/css/smallwindow.css" />
 
+   
+   
   <!-- FONT -->
   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
