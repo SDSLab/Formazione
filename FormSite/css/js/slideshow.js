ac95cf6097f9e94957dfac88a0dd279d67044b0b
CVarisco <C.Varisco@lutechmobile.com>
2014-05-09 17:59:33 +0200

    09/05 FORMSITE

--- FormSite/css/js/slideshow.js
+++ FormSite/css/js/slideshow.js
@@ -1,35 +1,98 @@
-var sliderGet =function (){
+var current=0;
+var next=0;
+var moviment=0;
+var transition=0;
 
-	var lengthLi=$('ul.slideMe li').length;
+var sliderGet ={
 
-	for (var i = 0; i < lengthLi; i++) {
-		$('.row.sliderIcon').append('<div class="circle"></div>');
-	}
+	init:function(){
 	
-	$('.circle:first-child').addClass('bgcircle');
-	$('ul.slideMe li').not('ul.slideMe li:first-child').addClass('slideEnter');
+		sliderGet.getCircle();
+		sliderGet.aniSlide();
 	
-	$('.circle').on('click', function(){
-		$('.circle.bgcircle').removeClass("bgcircle");
-		$(this).addClass("bgcircle");
+	},
+	
+	getCircle:function(){
+	
+		var lengthLi=$('ul.slideMe li').length;
+
+		for (var i = 0; i < lengthLi; i++) {
+			$('.row.sliderIcon').append('<div class="circle"></div>');
+			$('ul.slideMe li').eq(i).attr('data-indexLi', i);
+		}
 		
-		var index=$('.circle').index(this);
+		$('.circle:first-child').addClass('bgcircle');
 		
+		$('.circle').on('click', function(){
+			$('.circle.bgcircle').removeClass("bgcircle");
+			$(this).addClass("bgcircle");
+			});
+			
+			
 		
-			$('ul.slideMe li').eq(index).prevAll().addClass("slideExit").removeClass('slideEnter').delay(1000)
-                       .queue(function() {
-							
-							
-							
-						
-					   
-                          
-                       });
-					   
-		
-		
-	});
+	},
+	
+	aniSlide:function(){
+	
+	$('ul.slideMe li:not(ul.slideMe li:first-child)').addClass('slideEnter');
+	$('ul.slideMe li:first-child').addClass('slideCenter');
 	
 	
-}
-$(document).ready(sliderGet);
+	
+
+	
+		$('.circle').on('click', function(){
+		
+			next=$('.circle').index(this);
+			current=$('ul.slideMe li.slideCenter').attr('data-indexLi');
+			
+			if(next>current){
+		
+				moviment=1;
+				transition=('slideExit');
+				}else{
+				
+				moviment=0;
+				transition=('slideEnter');
+			}
+			console.log(current+" "+next+" "+moviment);
+			
+			
+			var i=0;
+			
+		while(current<next){
+			
+			
+			
+			
+			
+			
+			$('ul.slideMe li[data-indexLi="'+current+'"]').addClass(transition).removeClass('slideCenter').delay(500).queue(function(){
+			
+			
+			
+			$('ul.slideMe li[data-indexLi="'+next+'"]').delay(500).addClass('slideCenter').queue(function(){
+				
+				$('ul.slideMe li[data-indexLi="'+next+'"]').removeClass('slideEnter slideExit');
+			});;
+				});;
+				current++;
+				
+			
+		
+		
+		
+		}
+			
+			
+		});
+		
+		
+		
+	},
+	
+};
+	
+	
+
+$(document).ready(sliderGet.init);
